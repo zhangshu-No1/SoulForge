bb3a11e4da54173e735709e1ac3d6d5977f98809
zhangshu-No1 <34325768+zhangshu-No1@users.noreply.github.com>
2026-05-19 04:39:32 +0000

    feat: Phase1 提示词模板系统 + 适配器架构重构

---
+++ soulforge/core/__init__.py
@@ -0,0 +1,30 @@
+"""SoulForge Core Modules.
+
+Core modules for the SoulForge framework.
+"""
+
+from .memory_engine import MemoryEngine, MemoryEntry
+from .relationship import (
+    RelationshipManager,
+    GROWTH_STAGES,
+    PRESET_STAGES,
+    GrowthStage,
+)
+from .goal_keeper import GoalKeeper, Goal, BABY_STAGES
+from .baby_project import BabyProject
+from .prompt_templates import PromptTemplate, PromptTemplateManager
+
+__all__ = [
+    "MemoryEngine",
+    "MemoryEntry",
+    "RelationshipManager",
+    "GROWTH_STAGES",
+    "PRESET_STAGES",
+    "GrowthStage",
+    "GoalKeeper",
+    "Goal",
+    "BABY_STAGES",
+    "BabyProject",
+    "PromptTemplate",
+    "PromptTemplateManager",
+]
